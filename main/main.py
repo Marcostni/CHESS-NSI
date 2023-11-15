d452d8c67061de26408ce2d644b85422efdb1749
Byhrox <114606386+bYHROX@users.noreply.github.com>
2023-11-15 09:41:31 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -33,6 +33,37 @@ class Pion:
 
         return deplacements
 
+
+class Tour: 
+    def __init__(self, couleur, position, tableau):
+        self.couleur = couleur
+        self.position = position
+        self.chessboard = tableau
+
+    def deplacements_possibles(self):
+        deplacements = []
+        if self.couleur == "w":
+            if 97<ord(self.chessboard[self.position[0] - 1][self.position[1] + 1])<122 :
+                deplacements.append((self.position[0] - 1, self.position[1] + 1))
+            if 97<ord(self.chessboard[self.position[0] - 1][self.position[1] - 1])<122:
+                deplacements.append((self.position[0] - 1, self.position[1] - 1))
+            if self.position[0] - 1 >= 0 and self.chessboard[self.position[0] - 1][self.position[1]] == " ":
+                deplacements.append((self.position[0] - 1, self.position[1]))
+            if self.position[0] == 6 and self.chessboard[self.position[0] - 2][self.position[1]] == " ":
+                deplacements.append((self.position[0] - 2, self.position[1]))
+        
+        elif self.couleur == "b":
+            if 65<ord(self.chessboard[self.position[0] + 1][self.position[1] + 1])<90 :
+                deplacements.append((self.position[0] + 1, self.position[1] + 1))
+            if 65<ord(self.chessboard[self.position[0] + 1][self.position[1] - 1])<90:
+                deplacements.append((self.position[0] + 1, self.position[1] - 1))
+            if self.position[0] + 1 <= 7 and self.chessboard[self.position[0] + 1][self.position[1]] == " ":
+                deplacements.append((self.position[0] + 1, self.position[1]))
+            if self.position[0] == 1 and self.chessboard[self.position[0] + 2][self.position[1]] == " ":
+                deplacements.append((self.position[0] + 2, self.position[1]))
+
+        return deplacements
+
 class Fou:
     def __init__(self, couleur, position, tableau):
         self.couleur = couleur 
