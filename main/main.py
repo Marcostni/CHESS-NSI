ff791b3d23c68e124f58c054e267a1f839f2229d
vavalt8313 <150896844+vavalt8313@users.noreply.github.com>
2023-11-15 09:55:36 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -33,6 +33,35 @@ class Pion:
 
         return deplacements
 
+class Cavalier:
+    def __init__(self, couleur, position, tableau):
+        self.couleur = couleur
+        self.position = position
+        self.chessboard = tableau
+    
+    def deplacements_possibles(self):
+        deplacements = []
+        if self.couleur == "w":
+            if self.position[0] - 2 >= 0:
+                if self.position[1] - 1 >= 0 and (97<ord(self.chessboard[self.position[0] - 2][self.position[1] - 1])<122 or self.chessboard[self.position[0] - 2][self.position[1] - 1] == " " ):
+                    deplacements.append((self.position[0] - 2, self.position[1] - 1))
+                if self.position[1] + 1 <= 7 and (97<ord(self.chessboard[self.position[0] - 2][self.position[1] + 1])<122 or self.chessboard[self.position[0] - 2][self.position[1] + 1] == " " ):
+                    deplacements.append((self.position[0] - 2, self.position[1] + 1))
+            if self.position[0] + 2 <= 7:
+                if self.position[1] - 1 >= 0 and (97<ord(self.chessboard[self.position[0] + 2][self.position[1] - 1])<122 or self.chessboard[self.position[0] + 2][self.position[1] - 1] == " " ):
+                    deplacements.append((self.position[0] + 2, self.position[1] - 1))
+                if self.position[1] + 1 <= 7 and (97<ord(self.chessboard[self.position[0] + 2][self.position[1] + 1])<122 or self.chessboard[self.position[0] + 2][self.position[1] + 1] == " " ):
+                    deplacements.append((self.position[0] + 2, self.position[1] + 1))
+            if self.position[1] - 2 >= 0:
+                if self.position[0] - 1 >= 0 and (97<ord(self.chessboard[self.position[0] - 1][self.position[1] - 2])<122 or self.chessboard[self.position[0] - 2][self.position[1] - 1] == " " ):
+                    deplacements.append((self.position[0] - 2, self.position[1] - 1))
+                if self.position[1] + 1 <= 7 and (97<ord(self.chessboard[self.position[0] - 2][self.position[1] + 1])<122 or self.chessboard[self.position[0] - 2][self.position[1] + 1] == " " ):
+                    deplacements.append((self.position[0] - 2, self.position[1] + 1))
+            if self.position[0] + 2 <= 7:
+                if self.position[1] - 1 >= 0 and (97<ord(self.chessboard[self.position[0] + 2][self.position[1] - 1])<122 or self.chessboard[self.position[0] + 2][self.position[1] - 1] == " " ):
+                    deplacements.append((self.position[0] + 2, self.position[1] - 1))
+                if self.position[1] + 1 <= 7 and (97<ord(self.chessboard[self.position[0] + 2][self.position[1] + 1])<122 or self.chessboard[self.position[0] + 2][self.position[1] + 1] == " " ):
+                    deplacements.append((self.position[0] + 2, self.position[1] + 1))
 
 class Tour: 
     def __init__(self, couleur, position, tableau):
