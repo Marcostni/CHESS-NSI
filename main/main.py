673914105a63279e6970fed061ff4d3d9b1cf35a
Marcostni <150896085+Marcostni@users.noreply.github.com>
2023-11-19 14:15:59 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -100,6 +100,27 @@ class Fou:
         self.chessboard = tableau 
 
     def deplacemements_possibles(self):
+           deplacements = []
+        x, y = self.position
+
+        i = 1
+        while x - i >= 0 and y - i >= 0:
+            deplacements.append((x - i, y - i))
+            i += 1
+        i = 1
+        while x - i >= 0 and y + i < len(self.chessboard):
+            deplacements.append((x - i, y + i))
+            i += 1
+        i = 1
+        while x + i < len(self.chessboard) and y - i >= 0:
+            deplacements.append((x + i, y - i))
+            i += 1
+        i = 1
+        while x + i < len(self.chessboard) and y + i < len(self.chessboard):
+            deplacements.append((x + i, y + i))
+            i += 1
+
+        return deplacements
     
 class ChessGame:
     def __init__(self):
