68ededd70534e380e48541d2c303d554bc5e6ef1
vavalt8313 <150896844+vavalt8313@users.noreply.github.com>
2023-11-20 10:30:05 +0100

    Update pion.py

--- main/pion.py
+++ main/pion.py
@@ -8,10 +8,10 @@ class Pion:
         deplacements = []
         if self.couleur == "w":
             if self.position[1] + 1 <= 7:
-                if 97<ord(self.chessboard[self.position[0] - 1][self.position[1] + 1])<122 :
+                if self.chessboard[self.position[0] - 1][self.position[1] + 1].islower() :
                     deplacements.append((self.position[0] - 1, self.position[1] + 1))
             if self.position[1] - 1 >= 0:
-                if 97<ord(self.chessboard[self.position[0] - 1][self.position[1] - 1])<122:
+                if self.chessboard[self.position[0] - 1][self.position[1] - 1].islower():
                     deplacements.append((self.position[0] - 1, self.position[1] - 1))
             if self.position[0] - 1 >= 0 and self.chessboard[self.position[0] - 1][self.position[1]] == " ":
                 deplacements.append((self.position[0] - 1, self.position[1]))
@@ -20,7 +20,7 @@ class Pion:
         
         elif self.couleur == "b":
             if self.position[1] + 1 <= 7:
-                if 65<ord(self.chessboard[self.position[0] + 1][self.position[1] + 1])<90 :
+                if self.chessboard[self.position[0] + 1][self.position[1] + 1].isupper() :
                     deplacements.append((self.position[0] + 1, self.position[1] + 1))
             if self.position[1] - 1 >= 0:
                 if 65<ord(self.chessboard[self.position[0] + 1][self.position[1] - 1])<90:
