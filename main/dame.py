a4f2448abe0abb2db22449153790638c5d4636f8
Byhrox <114606386+bYHROX@users.noreply.github.com>
2023-11-21 20:39:24 +0100

    Update dame.py

--- main/dame.py
+++ main/dame.py
@@ -1,1 +1,16 @@
+class Dame:
+    
+    def __init__(self, couleur, position, tableau):
+        
+        self.couleur = couleur 
+        self.position = position
+        self.chessboard = tableau 
 
+    def deplacements_possibles(self):
+        from fou import Fou
+        from tour import Tour
+        fou_dep = Fou(self.couleur, self.position, self.chessboard).deplacements_possibles()
+        tour_dep = Tour(self.couleur, self.position, self.chessboard).deplacements_possibles()
+
+        deplacements = fou_dep + tour_dep
+        return deplacements
