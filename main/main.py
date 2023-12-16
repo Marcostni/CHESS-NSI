f97c710d20e6452ad207fbb8c15493c6ccdcd68f
vavalt8313 <150896844+vavalt8313@users.noreply.github.com>
2023-12-16 17:06:18 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -18,6 +18,13 @@ class ChessApp(App):
         self.chess_game = ChessGame()
         self.chessboard = self.root.ids.chessboard
         self.label = self.root.ids.label
+        self.lbl_pnt_b = self.root.ids.lbl_pnt_b
+        self.lbl_pnt_w = self.root.ids.lbl_pnt_w
+        self.prw = True 
+        self.grw = True
+        self.prb = True 
+        self.grb = True
+        self.roi = ""
         self.dico = {
             "p" : "ressources/bpion",
             "P" : "ressources/wpion", 
@@ -91,14 +98,31 @@ class ChessApp(App):
                     self.chess_game.piece = Roi("w", self.coordinates, self.chess_game.board)
                 self.possibility = self.chess_game.piece.deplacements_possibles()
                 self.position = self.coordinates
-                print(self.possibility)                
-            
+                print(self.possibility)
+                if self.letter == "r" or self.letter == "R":
+                    self.roi = self.letter
+
+
         else :
             
             if self.coordinates in self.possibility:
                 self.chess_game.make_move(self.position, self.coordinates)
                 pos_roi = self.chess_game.find_king()
                 if not self.chess_game.is_check(pos_roi):
+                    if self.letter == "r":
+                        self.prb = False
+                        self.grb = False
+                    elif self.letter == "R":
+                        self.prw = False
+                        self.grw = False
+                    elif self.position == (0,0):
+                        self.grb = False
+                    elif self.position == (0,7):
+                        self.prb = False
+                    elif self.position == (7,0):
+                        self.grw = False
+                    elif self.position == (7,7):
+                        self.prw = False
                     self.chessboard.clear_widgets()
                     self.create_chessboard()
                     if self.chess_game.current_player == "w" :
@@ -108,14 +132,134 @@ class ChessApp(App):
                         self.chess_game.current_player = "w"
                         self.label.text = "C'est aux blancs de jouer"
                     if self.chess_game.is_checkmate():
-                        print("checkmate")
                         if self.chess_game.current_player == "w" :
                             self.label.text = "Echec et mat. Les noirs ont gagné."
                         else:
                             self.label.text = "Echec et mat. Les blancs ont gagné."
+                    self.lbl_pnt_b.text = str(self.chess_game.pnts_b)
+                    self.lbl_pnt_w.text = str(self.chess_game.pnts_w)
                 else :
                     self.chess_game.undo_moove(self.coordinates, self.position)
-                
+
+            elif self.roi:
+                if not self.chess_game.is_check(self.position):
+                    if self.coordinates == (0, 0) and self.roi.islower():
+                        if self.grb:
+                            if self.chess_game.board[0][1] == " " and self.chess_game.board[0][2] == " " and self.chess_game.board[0][3] == " " :
+                                self.chess_game.make_move(self.position,(self.position[0], self.position[1] - 1))
+                                if not self.chess_game.is_check((self.position[0], self.position[1] - 1)):
+                                    self.chess_game.make_move((self.position[0], self.position[1] - 1),(self.position[0], self.position[1] - 2))
+                                    if not self.chess_game.is_check((self.position[0], self.position[1] - 2)):
+                                        self.chess_game.make_move(self.coordinates,(self.position[0], self.position[1] - 1))
+                                        self.prb = False
+                                        self.grb = False
+                                        self.chessboard.clear_widgets()
+                                        self.create_chessboard()
+                                        if self.chess_game.current_player == "w" :
+                                            self.chess_game.current_player = "b"
+                                            self.label.text = "C'est aux noirs de jouer"
+                                        else:
+                                            self.chess_game.current_player = "w"
+                                            self.label.text = "C'est aux blancs de jouer"
+                                        if self.chess_game.is_checkmate():
+                                            if self.chess_game.current_player == "w" :
+                                                self.label.text = "Echec et mat. Les noirs ont gagné."
+                                            else:
+                                                self.label.text = "Echec et mat. Les blancs ont gagné."
+                                        self.lbl_pnt_b.text = str(self.chess_game.pnts_b)
+                                        self.lbl_pnt_w.text = str(self.chess_game.pnts_w)
+                                    else:
+                                        self.chess_game.undo_move((self.position[0], self.position[1] - 2),(self.position[0], self.position[1] - 1))
+                                else:
+                                    self.chess_game.undo_moove((self.position[0], self.position[1] - 1), self.position)
+
+                    elif self.coordinates == (0, 7) and self.roi.islower():
+                        if self.prb:
+                            if self.chess_game.board[0][6] == " " and self.chess_game.board[0][5] == " " :
+                                self.chess_game.make_move(self.position,(self.position[0], self.position[1] + 1))
+                                if not self.chess_game.is_check((self.position[0], self.position[1] + 1)):
+                                    self.chess_game.make_move((self.position[0], self.position[1] + 1),(self.position[0], self.position[1] + 2))
+                                    if not self.chess_game.is_check((self.position[0], self.position[1] + 2)):
+                                        self.chess_game.make_move(self.coordinates,(self.position[0], self.position[1] + 1))
+                                        self.prb = False
+                                        self.grb = False
+                                        self.chessboard.clear_widgets()
+                                        self.create_chessboard()
+                                        if self.chess_game.current_player == "w" :
+                                            self.chess_game.current_player = "b"
+                                            self.label.text = "C'est aux noirs de jouer"
+                                        else:
+                                            self.chess_game.current_player = "w"
+                                            self.label.text = "C'est aux blancs de jouer"
+                                        if self.chess_game.is_checkmate():
+                                            if self.chess_game.current_player == "w" :
+                                                self.label.text = "Echec et mat. Les noirs ont gagné."
+                                            else:
+                                                self.label.text = "Echec et mat. Les blancs ont gagné."
+                                        self.lbl_pnt_b.text = str(self.chess_game.pnts_b)
+                                        self.lbl_pnt_w.text = str(self.chess_game.pnts_w)
+                                    else:
+                                        self.chess_game.undo_move((self.position[0], self.position[1] + 2),(self.position[0], self.position[1] + 1))
+                                else:
+                                    self.chess_game.undo_moove((self.position[0], self.position[1] + 1), self.position)
+
+                    elif self.coordinates == (7, 7) and not self.roi.islower():
+                        if self.prw:
+                            if self.chess_game.board[7][6] == " " and self.chess_game.board[7][5] == " " :
+                                self.chess_game.make_move(self.position,(self.position[0], self.position[1] + 1))
+                                if not self.chess_game.is_check((self.position[0], self.position[1] + 1)):
+                                    self.chess_game.make_move((self.position[0], self.position[1] + 1),(self.position[0], self.position[1] + 2))
+                                    if not self.chess_game.is_check((self.position[0], self.position[1] + 2)):
+                                        self.chess_game.make_move(self.coordinates,(self.position[0], self.position[1] + 1))
+                                        self.prw = False
+                                        self.grw = False
+                                        self.chessboard.clear_widgets()
+                                        self.create_chessboard()
+                                        if self.chess_game.current_player == "w" :
+                                            self.chess_game.current_player = "b"
+                                            self.label.text = "C'est aux noirs de jouer"
+                                        else:
+                                            self.chess_game.current_player = "w"
+                                            self.label.text = "C'est aux blancs de jouer"
+                                        if self.chess_game.is_checkmate():
+                                            if self.chess_game.current_player == "w" :
+                                                self.label.text = "Echec et mat. Les noirs ont gagné."
+                                            else:
+                                                self.label.text = "Echec et mat. Les blancs ont gagné."
+                                    else:
+                                        self.chess_game.undo_move((self.position[0], self.position[1] + 2),(self.position[0], self.position[1] + 1))
+                                else:
+                                    self.chess_game.undo_moove((self.position[0], self.position[1] + 1), self.position)
+
+                    elif self.coordinates == (7, 0) and not self.roi.islower():
+                        if self.grw:
+                            if self.chess_game.board[7][1] == " " and self.chess_game.board[7][2] == " " and self.chess_game.board[7][3] == " " :
+                                self.chess_game.make_move(self.position,(self.position[0], self.position[1] - 1))
+                                if not self.chess_game.is_check((self.position[0], self.position[1] - 1)):
+                                    self.chess_game.make_move((self.position[0], self.position[1] - 1),(self.position[0], self.position[1] - 2))
+                                    if not self.chess_game.is_check((self.position[0], self.position[1] - 2)):
+                                        self.chess_game.make_move(self.coordinates,(self.position[0], self.position[1] - 1))
+                                        self.prw = False
+                                        self.grw = False
+                                        self.chessboard.clear_widgets()
+                                        self.create_chessboard()
+                                        if self.chess_game.current_player == "w" :
+                                            self.chess_game.current_player = "b"
+                                            self.label.text = "C'est aux noirs de jouer"
+                                        else:
+                                            self.chess_game.current_player = "w"
+                                            self.label.text = "C'est aux blancs de jouer"
+                                        if self.chess_game.is_checkmate():
+                                            if self.chess_game.current_player == "w" :
+                                                self.label.text = "Echec et mat. Les noirs ont gagné."
+                                            else:
+                                                self.label.text = "Echec et mat. Les blancs ont gagné."
+                                    else:
+                                        self.chess_game.undo_move((self.position[0], self.position[1] - 2),(self.position[0], self.position[1] - 1))
+                                else:
+                                    self.chess_game.undo_moove((self.position[0], self.position[1] - 1), self.position)
+
+                self.roi = ""
             self.chess_game.piece = ""
 
     def start_new_game(self):
@@ -123,6 +267,8 @@ class ChessApp(App):
         self.chessboard.clear_widgets()
         self.create_chessboard()
         self.label.text = "C'est aux blancs de jouer"
+        self.lbl_pnt_b.text = str(self.chess_game.pnts_b)
+        self.lbl_pnt_w.text = str(self.chess_game.pnts_w)
 
     def leave(self):
         App.get_running_app().stop()
